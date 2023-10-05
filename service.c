5dc6b455553ed6af2358e15c7e5ff2f29b7b0e22
Ratapumin Naudom <ratapumin@gmail.com>
2023-10-05 20:31:47 +0700

    check values compare in csv success

--- service.c
+++ service.c
@@ -4,9 +4,10 @@
 #define maxSize 20
 void service(int isService, customers *current)
 {
-    int case_service;
+    int case_service, case_time;
     int day, month, year;
-    int hour, minute;
+    float add_time;
+    float save_time;
 
     while (1)
     {
@@ -24,29 +25,80 @@ void service(int isService, customers *current)
         {
         case 1:
         {
-            FILE *servicefile;
-            servicefile = fopen("service.csv", "a");
-            if (!servicefile)
-            {
-                perror("Can not opening customer.csv!");
-                exit(1);
-            }
-            printf("===============================\n");
-            printf("Braces\n");
-            printf("Day (01/xx/xxxx): ");
-            scanf("%d", &day);
-            printf("Month (xx/01/xxxx): ");
-            scanf("%d", &month);
-            printf("Year (xx/xx/2001): ");
-            scanf("%d", &year);
-            printf("Hour: ");
-            scanf("%d", &hour);
-            printf("Minute:");
-            scanf("%d", &minute);
 
-            fprintf(servicefile, "%s,%s,%d,%d,%d,%d,%d\n", customerData.id_card, customerData.username, day, month, year, hour, minute);
-            printf("Booking Successfully!!!\n");
-            fclose(servicefile);
+            system("cls");
+            printf("===============================\n");
+            printf("Plase Choose the desired time\n");
+            printf("Time 10.00 - 19.00\n");
+            printf("1: 10.00\n");
+            printf("2: 10.30\n");
+            printf("3: 11.00\n");
+            printf("4: 11.30\n");
+            printf("5: 13.00\n");
+            printf("6: 13.30\n");
+            printf("7: 14.00\n");
+            printf("8: 14.30\n");
+            printf("9: 15.30\n");
+            printf("10: 16.00\n");
+            printf("11: 16.30\n");
+            printf("12: 17.00\n");
+            printf("13: 17.30\n");
+            printf("14: 18.00\n");
+            printf("15: 18.30\n");
+            printf("16: 19.00\n");
+            printf("Please select a time period: ");
+            scanf("%d", &case_time);
+
+            switch (case_time)
+            {
+            case 1:
+            {
+
+                add_time = 10.00;
+                FILE *servicefile;
+                servicefile = fopen("service.csv", "r");
+                if (!servicefile)
+                {
+                    perror("Can not opening customer.csv!");
+                    exit(1);
+                }
+                int found = 0;
+                char line[1024];
+                while (fgets(line, sizeof(line), servicefile))
+                {
+                    sscanf(line, "%[^,],%[^,],%f", customerData.id_card, customerData.username, &save_time);
+                    if (add_time == save_time)
+                    {
+                        found = 1;
+                        printf("%d\n", found);
+                        printf("This time slot has been reserved.\n");
+                        break;
+                    }
+                }
+                fclose(servicefile);
+
+                if (!found)
+                {
+
+                    printf("%d\n", found);
+                    FILE *servicefile;
+                    servicefile = fopen("service.csv", "a");
+                    if (!servicefile)
+                    {
+                        perror("Error opening file");
+                        exit(1);
+                    }
+                    fprintf(servicefile, "%s,%s,%.2f\n", customerData.id_card, customerData.username, add_time);
+                    printf("Booking Successfully!!!\n");
+                    printf("%.2f\n", add_time);
+                    fclose(servicefile);
+                }
+            }
+            break;
+
+            default:
+                break;
+            }
             break;
         }
         break;
